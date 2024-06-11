71c2d20d4f3e8fff9f8b27321f50861c1ac67e72
tedsalvador <tedpaul@hotmail.com>
2024-06-11 10:20:12 +0200

    creando componente calculator

---
+++ src/components/calculator.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+</script>
+
+<template>
+   <div class="calculator">
+     <div id="screen"></div>
+     <div class="btn">7</div>
+     <div class="btn">8</div>
+     <div class="btn">9</div>
+     <div class="btn operator">+</div>
+
+     <div class="btn">4</div>
+     <div class="btn">5</div>
+     <div class="btn">6</div>
+     <div class="btn operator">-</div>
+
+     <div class="btn">3</div>
+     <div class="btn">2</div>
+     <div class="btn">1</div>
+     <div class="btn operator">*</div>
+
+     <div class="btn operator">C</div>
+     <div class="btn">0</div>
+     <div class="btn operator">=</div>
+     <div class="btn operator">/</div>
+   </div>
+</template>
+
+<style lang="scss" scoped>
+.btn {
+ background-color: grey;
+ color: white;
+ font-size: 24px;
+
+ .calculator {
+    display: flex;    
+ }
+}
+
+</style>
